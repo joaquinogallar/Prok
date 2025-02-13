83ce3e947cd0fa2919b87db23412849240fc6159
joaco <joaquinnogallar@gmail.com>
2025-02-13 13:54:37 -0300

    feat: controller for the view and login page

---
+++ src/main/java/com/joaquinogallar/prok/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.joaquinogallar.prok.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/loginTest")
+    public String login() {
+        return "login";
+    }
+}
