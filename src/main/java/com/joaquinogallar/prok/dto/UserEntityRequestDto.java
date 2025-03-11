1a0dc05bf64096ad4e104468856f9eebbd2d88f8
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-03-11 18:38:15 -0300

    feat: inputa validation for register form

--- src/main/java/com/joaquinogallar/prok/dto/UserEntityRequestDto.java
+++ src/main/java/com/joaquinogallar/prok/dto/UserEntityRequestDto.java
@@ -1,5 +1,8 @@
 package com.joaquinogallar.prok.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,14 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class UserEntityRequestDto {
 
+    @NotBlank(message = "Fullname cannot be empty.")
     private String fullName;
+
+    @NotBlank(message = "Email cannot be empty.")
+    @Email(message = "The email is incorrect.")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.")
+    @Size(min = 8, message = "Password must be at least 6 characters long.")
     private String password;
 }
--- src/main/java/com/joaquinogallar/prok/dto/UserLoginDto.java
+++ src/main/java/com/joaquinogallar/prok/dto/UserLoginDto.java
@@ -13,10 +13,8 @@ import lombok.NoArgsConstructor;
 public class UserLoginDto {
 
     @NotBlank(message = "Email cannot be empty.")
-    @Email(message = "The email is incorrect.")
     private String email;
 
     @NotBlank(message = "Password cannot be empty.")
-    @Size(min = 8, message = "Password must be at least 6 characters long")
     private String password;
 }
