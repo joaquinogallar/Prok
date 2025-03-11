ed5933e9e222e43fb7d0ab8709ba8a6b5a1b9771
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-03-11 18:34:32 -0300

    feat: validation input for login

--- src/main/java/com/joaquinogallar/prok/dto/UserLoginDto.java
+++ src/main/java/com/joaquinogallar/prok/dto/UserLoginDto.java
@@ -1,5 +1,8 @@
 package com.joaquinogallar.prok.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginDto {
+
+    @NotBlank(message = "Email cannot be empty.")
+    @Email(message = "The email is incorrect.")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.")
+    @Size(min = 8, message = "Password must be at least 6 characters long")
     private String password;
 }
