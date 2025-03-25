06a1aef8efe673ca76f72047a26cb633037f4859
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-03-25 07:33:15 -0300

    feat: add redirection to login when user is unauthorized

--- src/main/java/com/joaquinogallar/prok/config/SecurityConfig.java
+++ src/main/java/com/joaquinogallar/prok/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -28,6 +30,9 @@ public class SecurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -38,6 +43,9 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
@@ -47,7 +55,9 @@ public class SecurityConfig {
                             cookie.setMaxAge(0);
                             response.addCookie(cookie);
                         })
-                        .logoutSuccessUrl("/auth/login?logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.sendRedirect("/auth/login?logout");
+                        })
                         .permitAll()
                 )
                 .sessionManagement(sessionManagement -> sessionManagement
@@ -58,5 +68,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
